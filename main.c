9fe094816e539eb6c820739eb4503857e6a53439
Said Bora Ozdamar <sozdamar@k1m35s10.42istanbul.com.tr>
2023-12-02 14:51:34 +0300

    another test added

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
-
+/*	
 int main()
 {
 	char	*line;
@@ -20,7 +20,19 @@ int main()
 	close(fd);
 }
 
-/*
 check for fd = 0, 1, 2...
 study buffer, static variable and functions
 */
+
+int main()
+{
+	char	*line;
+	int		fd, row;
+	int 	i = 4;
+
+	fd = open("test.txt", O_RDONLY);
+	while (i--)
+		line = get_next_line(fd);
+	printf("%s", line);
+	free(line);
+}
